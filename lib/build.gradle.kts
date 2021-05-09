c955db8f85e3c9b18eaa2ff8277b44a97d7efba4
縣 <ymgtech@millenary.net>
2021-05-09 12:59:02 +0900

    [change] Google Guava の Base32 コーダーが Crockford 製ではなく、面倒なので文字列そのままで保存するよう変更

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,7 +1,6 @@
 object Versions {
     const val exposed = "0.31.1"
     const val kulid = "1.1.2.0"
-    const val guava = "30.1.1-jre"
 }
 
 plugins {
@@ -25,9 +24,6 @@ dependencies {
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    // Google Guava
-    implementation("com.google.guava:guava:${Versions.guava}")
 
     // Exposed
     api("org.jetbrains.exposed:exposed-core:${Versions.exposed}")
--- lib/src/main/kotlin/net/millenary/exposed/sql/ULIDColumnType.kt
+++ lib/src/main/kotlin/net/millenary/exposed/sql/ULIDColumnType.kt
@@ -1,7 +1,6 @@
 package net.millenary.exposed.sql
 
 import com.github.guepardoapps.kulid.ULID
-import com.google.common.io.BaseEncoding
 import org.jetbrains.exposed.sql.ColumnType
 
 /**
@@ -11,20 +10,19 @@ class ULIDColumnType: ColumnType() {
 
     override fun nonNullValueToString(value: Any): String = valueToUlid(value)
 
-    override fun notNullValueToDB(value: Any): Any = BaseEncoding.base32().decode(valueToUlid(value))
+    override fun notNullValueToDB(value: Any): Any = valueToUlid(value)
 
     override fun sqlType(): String = dataType
 
     override fun valueFromDB(value: Any): String = valueToUlid(value)
 
     private fun valueToUlid(value: Any): String = when {
-        value is ByteArray -> ULID.fromString(BaseEncoding.base32().encode(value))
         value is String && ULID.isValid(value) -> value
         value is String -> ULID.fromString(value)
         else -> error("Unexpected value of type ULID: $value of ${value::class.qualifiedName}")
     }
 
     companion object {
-        const val dataType: String = "BINARY(16)"
+        const val dataType: String = "VARCHAR(26)"
     }
 }
